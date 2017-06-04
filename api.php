322d06bd0e7c483001d4f885b00a0fd6bbbafe45
Amadeusz Kryze <amadeusz.kryze@gmail.com>
2017-06-04 10:42:07 +0200

    Added basic ajax

--- api.php
+++ api.php
@@ -1,11 +1,2 @@
 <?php
-$json = '{"książki": [{"tytuł": "T1","liczba_stron": 10,"dostępna": false}, {"tytuł": "T2","liczba_stron": 50,"dostępna": true} ]}';
-$var = json_decode($json);
-var_dump($json);
-var_dump($var);
-
-var_dump($var->książki[0]->liczba_stron);
-
-$dane = ['książki' => [['title' => 'T1', 'liczba_stron' => 12], ['title' => 'T2', 'liczba_stron' => 12]]];
-var_dump($dane);
-var_dump(json_encode($dane));
+echo json_encode([1,2,3]);
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 sudo php -S localhost:8000
+sudo php -S localhost:8000 &
