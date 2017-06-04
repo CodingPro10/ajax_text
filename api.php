351f9466e2c6cc44809b7fb3bcb4d613974f80b7
Amadeusz Kryze <amadeusz.kryze@gmail.com>
2017-06-04 11:31:19 +0200

    Added multiple http methods

--- api.php
+++ api.php
@@ -1,2 +1,13 @@
 <?php
-echo json_encode([rand(0,9),rand(0,9),rand(0,9)]);
+
+switch($_SERVER['REQUEST_METHOD']){
+    case 'GET': {
+        echo json_encode([rand(0,9),rand(0,9),rand(0,9)]);
+    } break;
+    case 'PUT': {
+        echo json_encode([rand(10,99),rand(10,99),rand(10,99)]);
+    } break;
+    case 'DELETE': {
+        echo json_encode([rand(100,999),rand(100,999),rand(100,999)]);
+    } break;
+}
